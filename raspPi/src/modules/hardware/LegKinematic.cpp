af3341bcf292b2faa45b90f5f5f868ec4f05c387
Nicolas <Nicolas@aa.bb>
2017-03-24 17:33:50 +0100

    untabified code

--- raspPi/src/modules/hardware/LegKinematic.cpp
+++ raspPi/src/modules/hardware/LegKinematic.cpp
@@ -8,190 +8,190 @@
 using namespace std;
 using namespace Eigen;
 
-LegKinematic::LegKinematic() 
-		: d1(90.0f), d2(125.0f), d3(150.0f), test{0.0f, 2}
+LegKinematic::LegKinematic()
+    : d1(90.0f), d2(125.0f), d3(150.0f), test{ 0.0f, 2 }
 {
 }
- 
+
 LegKinematic::~LegKinematic()
 {
 }
 
 void LegKinematic::setGoal(float goalX, float goalY, float goalZ)
 {
-	// Solve the inverse kinematic for the 3 DOF leg.
-	
-	float jointAngles[3];
-	// Joint 1 angle is given by:
-	jointAngles[0] = atan2(goalY, goalX);
-	// Find the end of link 1.
-	// Matrix4f jointT; // Joint  transform.
-	// jointTransform(jointT, 0.0f, m_joint1Angle_rad);
-	// Matrix4f linkT; // Link transform.
-	// linkTransform(linkT, d1, M_PI/2.0f);	
-	Matrix4f jointLinkT; // = jointT * linkT;
-	jointLinkTransform(jointLinkT, 0.0f, jointAngles[0], d1, M_PI/2.0f);
+    // Solve the inverse kinematic for the 3 DOF leg.
 
-	invertTransform(jointLinkT);
-	// Transform goal position into end of link 1 franejoint 1 frame.
-	Vector4f goal;
-	goal << goalX, goalY, goalZ, 1.0f;
-	goal = jointLinkT * goal;
-	
-	// The transformed goal point have a z value of zero.
-	// Due to the geometry of the 3 DOF leg, the problem is now reduced to finding the intersection of two circles.
-	// One is centered in (0,0) end of Link 1  the other centered around the transformed goal point.
-	
-	// http://paulbourke.net/geometry/circlesphere/
-	float x, y, x2, y2, x3, y3;
-	x = goal(0);
-	y = goal(1);
-	float dist = hypot(x,y);
-	if( dist > (d2 + d3) || dist < fabs(d2 - d3) || dist == 0.0f)
-	{ 
-		cout << "Goal un-reachable.";
-		return;
-	}
-	
-	float a = ((d2*d2) - (d3*d3) + (dist*dist))/(2.0f*dist);
-	if(d2 == a) 
-	{ 
-		cout << "Goal has only one solution.";
-	}		
-	float h = sqrtf(d2*d2 - a*a);
-	x2 = x * a / dist;
-	y2 = y * a / dist;
+    float jointAngles[3];
+    // Joint 1 angle is given by:
+    jointAngles[0] = atan2(goalY, goalX);
+    // Find the end of link 1.
+    // Matrix4f jointT; // Joint  transform.
+    // jointTransform(jointT, 0.0f, m_joint1Angle_rad);
+    // Matrix4f linkT; // Link transform.
+    // linkTransform(linkT, d1, M_PI/2.0f);	
+    Matrix4f jointLinkT; // = jointT * linkT;
+    jointLinkTransform(jointLinkT, 0.0f, jointAngles[0], d1, M_PI / 2.0f);
+
+    invertTransform(jointLinkT);
+    // Transform goal position into end of link 1 franejoint 1 frame.
+    Vector4f goal;
+    goal << goalX, goalY, goalZ, 1.0f;
+    goal = jointLinkT * goal;
+
+    // The transformed goal point have a z value of zero.
+    // Due to the geometry of the 3 DOF leg, the problem is now reduced to finding the intersection of two circles.
+    // One is centered in (0,0) end of Link 1  the other centered around the transformed goal point.
+
+    // http://paulbourke.net/geometry/circlesphere/
+    float x, y, x2, y2, x3, y3;
+    x = goal(0);
+    y = goal(1);
+    float dist = hypot(x, y);
+    if (dist > (d2 + d3) || dist < fabs(d2 - d3) || dist == 0.0f)
+    {
+        cout << "Goal un-reachable.";
+        return;
+    }
+
+    float a = ((d2*d2) - (d3*d3) + (dist*dist)) / (2.0f*dist);
+    if (d2 == a)
+    {
+        cout << "Goal has only one solution.";
+    }
+    float h = sqrtf(d2*d2 - a*a);
+    x2 = x * a / dist;
+    y2 = y * a / dist;
 
 
-	// Joint 2 angle is given by:
-	// x3 = x2 + h * y / dist;
-	// y3 = y2 - h * x / dist;
-	// jointAngles[1] = atan2(y3, x3);
-	// Or tbe other solution ...
-	x3 = x2 - h * y / dist;
-	y3 = y2 + h * x / dist;
-	jointAngles[1] = atan2(y3, x3);
-	//cout << "m_joint2Angle_rad: " << RAD2DEG(m_joint2Angle_rad) << std::endl;	
-	
-	// Transform the goal point into the coordinate frame of link 2.
-	jointLinkTransform(jointLinkT, 0.0f, jointAngles[1], d2, 0.0f);
-	invertTransform(jointLinkT);
-	goal = jointLinkT * goal;
-	
-	// x = x - x3;
-	// y = y - y3;
-	// float s = sin(-jointAngles[1]);
-	// float c = cos(-jointAngles[1]);
-	// x = x*c - y*s;
-	// y = x*s + y*c;
-	jointAngles[2] =  atan2(goal(1), goal(0));
-	
-	m_joint1Angle_rad = jointAngles[0];
-	m_joint2Angle_rad = jointAngles[1];
-	m_joint3Angle_rad = jointAngles[2];
+    // Joint 2 angle is given by:
+    // x3 = x2 + h * y / dist;
+    // y3 = y2 - h * x / dist;
+    // jointAngles[1] = atan2(y3, x3);
+    // Or tbe other solution ...
+    x3 = x2 - h * y / dist;
+    y3 = y2 + h * x / dist;
+    jointAngles[1] = atan2(y3, x3);
+    //cout << "m_joint2Angle_rad: " << RAD2DEG(m_joint2Angle_rad) << std::endl;	
+
+    // Transform the goal point into the coordinate frame of link 2.
+    jointLinkTransform(jointLinkT, 0.0f, jointAngles[1], d2, 0.0f);
+    invertTransform(jointLinkT);
+    goal = jointLinkT * goal;
+
+    // x = x - x3;
+    // y = y - y3;
+    // float s = sin(-jointAngles[1]);
+    // float c = cos(-jointAngles[1]);
+    // x = x*c - y*s;
+    // y = x*s + y*c;
+    jointAngles[2] = atan2(goal(1), goal(0));
+
+    m_joint1Angle_rad = jointAngles[0];
+    m_joint2Angle_rad = jointAngles[1];
+    m_joint3Angle_rad = jointAngles[2];
 }
 
 void LegKinematic::getGoal(float& goalX, float& goalY, float& goalZ)
 {
-	/*
-	Denavit-Hartenberg description:
+    /*
+    Denavit-Hartenberg description:
 
-	Jd - Joint distance
-	Ja - Joint angle 
-	La - link angle
-	Ld - link distance
+    Jd - Joint distance
+    Ja - Joint angle
+    La - link angle
+    Ld - link distance
 
-	_____________
-	Joint	jd	Ja	La	Ld	
-	0		0	a1	90	d1	
-	1		0	a2	0	d2	
-	2		0	a3	0	d3
+    _____________
+    Joint   jd  Ja  La  Ld
+    0       0   a1  90  d1
+    1       0   a2  0   d2
+    2       0   a3  0   d3
 
-	*/	
-	float ja[3] = {m_joint1Angle_rad,  m_joint2Angle_rad,  m_joint3Angle_rad};
-	float jd[3] = {0};
-	float ld[3] = {d1, d2, d3};
-	float la[3] = {M_PI/2.0f,  0.0f, 0.0f};
-	
-	Matrix4f transform = Matrix4f::Identity();
-	for(int i = 0; i < 3; ++i)
-	{
-		Matrix4f jointLinkT;
-		jointLinkTransform(jointLinkT, jd[i], ja[i], ld[i], la[i]);
-		transform = transform * jointLinkT;
-		/* Debug output.
-		std::cout << "Link" << i << std::endl << std::endl;
-		std::cout << "Joint" << std::endl << jointT << std::endl;
-		std::cout << "Link" << std::endl << linkT << std::endl;
-		std::cout << "Transform" << std::endl << transform << std::endl << std::endl;
-		*/
+    */
+    float ja[3] = { m_joint1Angle_rad, m_joint2Angle_rad, m_joint3Angle_rad };
+    float jd[3] = { 0 };
+    float ld[3] = { d1, d2, d3 };
+    float la[3] = { M_PI / 2.0f, 0.0f, 0.0f };
 
-	}
-	goalX = transform(0,3);
-	goalY = transform(1,3);
-	goalZ = transform(2,3);
+    Matrix4f transform = Matrix4f::Identity();
+    for (int i = 0; i < 3; ++i)
+    {
+        Matrix4f jointLinkT;
+        jointLinkTransform(jointLinkT, jd[i], ja[i], ld[i], la[i]);
+        transform = transform * jointLinkT;
+        /* Debug output.
+        std::cout << "Link" << i << std::endl << std::endl;
+        std::cout << "Joint" << std::endl << jointT << std::endl;
+        std::cout << "Link" << std::endl << linkT << std::endl;
+        std::cout << "Transform" << std::endl << transform << std::endl << std::endl;
+        */
+
+    }
+    goalX = transform(0, 3);
+    goalY = transform(1, 3);
+    goalZ = transform(2, 3);
 }
 
 void LegKinematic::setJoints(float joint1Angle_deg, float joint2Angle_deg, float joint3Angle_deg)
 {
-	m_joint1Angle_rad = DEG2RAD(joint1Angle_deg);
-	m_joint2Angle_rad = DEG2RAD(joint2Angle_deg);
-	m_joint3Angle_rad = DEG2RAD(joint3Angle_deg);
+    m_joint1Angle_rad = DEG2RAD(joint1Angle_deg);
+    m_joint2Angle_rad = DEG2RAD(joint2Angle_deg);
+    m_joint3Angle_rad = DEG2RAD(joint3Angle_deg);
 }
 
 void LegKinematic::getJoints(float& joint1Angle_deg, float& joint2Angle_deg, float& joint3Angle_deg)
 {
-	joint1Angle_deg = RAD2DEG(m_joint1Angle_rad);
-	joint2Angle_deg = RAD2DEG(m_joint2Angle_rad);
-	joint3Angle_deg = RAD2DEG(m_joint3Angle_rad);
+    joint1Angle_deg = RAD2DEG(m_joint1Angle_rad);
+    joint2Angle_deg = RAD2DEG(m_joint2Angle_rad);
+    joint3Angle_deg = RAD2DEG(m_joint3Angle_rad);
 }
 float LegKinematic::getJoint(int idx)
 {
-	switch(idx)
-	{
-		case 0: return RAD2DEG(m_joint1Angle_rad);
-		case 1: return RAD2DEG(m_joint2Angle_rad);
-		case 2: return RAD2DEG(m_joint3Angle_rad);
-		default: return 0.0f;
-	}
+    switch (idx)
+    {
+    case 0: return RAD2DEG(m_joint1Angle_rad);
+    case 1: return RAD2DEG(m_joint2Angle_rad);
+    case 2: return RAD2DEG(m_joint3Angle_rad);
+    default: return 0.0f;
+    }
 }
 
 void LegKinematic::jointTransform(Matrix4f &jointT, float jd, float ja)
 {
-		float s = sin(ja);
-		float c = cos(ja);
-		jointT.setIdentity();
-		jointT(0,0) = c;
-		jointT(1,1) = c;
-		jointT(0,1) = -s;
-		jointT(1,0) = s;
-		jointT(2,3) = jd;
+    float s = sin(ja);
+    float c = cos(ja);
+    jointT.setIdentity();
+    jointT(0, 0) = c;
+    jointT(1, 1) = c;
+    jointT(0, 1) = -s;
+    jointT(1, 0) = s;
+    jointT(2, 3) = jd;
 }
 void LegKinematic::linkTransform(Matrix4f &linkT, float ld, float la)
 {
-		float s = sin(la);
-		float c = cos(la);
-		linkT.setIdentity();
-		linkT(1,1) = c;
-		linkT(2,2) = c;
-		linkT(1,2) = -s;
-		linkT(2,1) = s;
-		linkT(0,3) = ld;
+    float s = sin(la);
+    float c = cos(la);
+    linkT.setIdentity();
+    linkT(1, 1) = c;
+    linkT(2, 2) = c;
+    linkT(1, 2) = -s;
+    linkT(2, 1) = s;
+    linkT(0, 3) = ld;
 }
 void LegKinematic::jointLinkTransform(Eigen::Matrix4f &jointLinkT, float jd, float ja, float ld, float la)
 {
-	// Joint  transform.
-	jointTransform(jointLinkT, jd, ja);
-	Matrix4f linkT; // Link transform.
-	linkTransform(linkT, ld, la);	
-	// Combined transform.
-	jointLinkT = jointLinkT * linkT;
+    // Joint  transform.
+    jointTransform(jointLinkT, jd, ja);
+    Matrix4f linkT; // Link transform.
+    linkTransform(linkT, ld, la);
+    // Combined transform.
+    jointLinkT = jointLinkT * linkT;
 }
 
 void LegKinematic::invertTransform(Matrix4f &transform)
 {
-	transform.topLeftCorner<3,3>().transposeInPlace();
-	transform.topRightCorner<3,1>() = -1.0f * transform.topLeftCorner<3,3>() * transform.topRightCorner<3,1>();
+    transform.topLeftCorner<3, 3>().transposeInPlace();
+    transform.topRightCorner<3, 1>() = -1.0f * transform.topLeftCorner<3, 3>() * transform.topRightCorner<3, 1>();
 }
 
 
--- raspPi/src/modules/hardware/pca9685.cpp
+++ raspPi/src/modules/hardware/pca9685.cpp
@@ -9,51 +9,51 @@
 
 enum REG
 {
-	MODE1			= 0x00,
-	MODE2			= 0x01,
-	SUBADR1		= 0x02,
-	SUBADR2		= 0x03,
-	SUBADR3		= 0x04,
-	ALLCALLADR	= 0x05,
-	LED0_ON_L		= 0x06,
-	LED0_ON_H		= 0x07,
-	LED0_OFF_L		= 0x08,
-	LED0_OFF_H		= 0x09,
- 	ALL_LED_ON_L	= 0xFA,
- 	ALL_LED_ON_H	= 0xFB,
- 	ALL_LED_OFF_L	= 0xFC,
- 	ALL_LED_OFF_H	= 0xFD,
-	PRE_SCALE		= 0xFE,
-	TestMode		= 0xFF
+    MODE1           = 0x00,
+    MODE2           = 0x01,
+    SUBADR1         = 0x02,
+    SUBADR2         = 0x03,
+    SUBADR3         = 0x04,
+    ALLCALLADR      = 0x05,
+    LED0_ON_L       = 0x06,
+    LED0_ON_H       = 0x07,
+    LED0_OFF_L      = 0x08,
+    LED0_OFF_H      = 0x09,
+    ALL_LED_ON_L    = 0xFA,
+    ALL_LED_ON_H    = 0xFB,
+    ALL_LED_OFF_L   = 0xFC,
+    ALL_LED_OFF_H   = 0xFD,
+    PRE_SCALE       = 0xFE,
+    TestMode        = 0xFF
 };
 
 // Control register MODE1 bits
-#define RESTART		(7)
-#define EXTCLK		(6)
-#define AI			(5)
-#define SLEEP		(4)
-#define SUB1		(3)
-#define SUB2		(2)
-#define SUB3		(1)
-#define ALLCALL		(0)
+#define RESTART     (7)
+#define EXTCLK      (6)
+#define AI          (5)
+#define SLEEP       (4)
+#define SUB1        (3)
+#define SUB2        (2)
+#define SUB3        (1)
+#define ALLCALL     (0)
 
 // Control register MODE2 bits
-#define INVRT		(4)
-#define OCH			(3)
-#define OUTDRV		(2)
-#define OUTNE_1		(1)
-#define OUTNE_0		(0)
+#define INVRT       (4)
+#define OCH         (3)
+#define OUTDRV      (2)
+#define OUTNE_1     (1)
+#define OUTNE_0     (0)
 
 #define INT_OSC_FREQ (25000000.0f) // Hz
 #define CHANNEL_COUNT (16)
 #define BV(bit) (1<<(bit))  // Bit value.
-pca9685::pca9685() 
-	: m_file(-1)
+pca9685::pca9685()
+    : m_file(-1)
 {
-	for(int i =0; i < CHANNEL_COUNT; ++i)
-	{	// Turn off all PWM channels.
-		setPWM(i,0,0);
-	}
+    for (int i = 0; i < CHANNEL_COUNT; ++i)
+    {	// Turn off all PWM channels.
+        setPWM(i, 0, 0);
+    }
 }
 
 pca9685::~pca9685()
@@ -61,139 +61,140 @@ pca9685::~pca9685()
 }
 int pca9685::init()
 {
-	int adapter_nr = 1; /* probably dynamically determined */
-	char filename[20];
-  
-	snprintf(filename, 19, "/dev/i2c-%d", adapter_nr);
-	m_file = open(filename, O_RDWR);
-	if (m_file < 0) {
-		/* ERROR HANDLING; you can check errno to see what went wrong */
-		return m_file;
-	}
+    int adapter_nr = 1; /* probably dynamically determined */
+    char filename[20];
 
-	int addr = 0x70; /* The I2C address */
-	if (ioctl(m_file, I2C_SLAVE, addr) < 0) {
-		/* ERROR HANDLING; you can check errno to see what went wrong */
-		return m_file;
-  	}
+    snprintf(filename, 19, "/dev/i2c-%d", adapter_nr);
+    m_file = open(filename, O_RDWR);
+    if (m_file < 0) {
+        /* ERROR HANDLING; you can check errno to see what went wrong */
+        return m_file;
+    }
 
-	
-	__s32 mode1 = i2c_smbus_read_byte_data(m_file, MODE1);
-	if (mode1 < 0) { return mode1;}
-	// Set auto increment bit, clear sleep bit .
-	mode1 |= BV(AI);
-	mode1 &= ~BV(SLEEP);
-	//mode1 |= BV(RESTART);	
-	__s32 res = i2c_smbus_write_byte_data(m_file, MODE1,  mode1);
-	if (res < 0) { return res;}
-	return 0;
+    int addr = 0x70; /* The I2C address */
+    if (ioctl(m_file, I2C_SLAVE, addr) < 0) {
+        /* ERROR HANDLING; you can check errno to see what went wrong */
+        return m_file;
+    }
 
-	__s32 mode2 = i2c_smbus_read_byte_data(m_file, MODE2);
-	if (mode2 < 0) { return mode2;}
-	mode2 |= BV(OUTDRV);
-	res = i2c_smbus_write_byte_data(m_file, MODE2,  mode2);
-	if (res < 0) { return res;}
-	return 0;
+
+    __s32 mode1 = i2c_smbus_read_byte_data(m_file, MODE1);
+    if (mode1 < 0) { return mode1; }
+    // Set auto increment bit, clear sleep bit .
+    mode1 |= BV(AI);
+    mode1 &= ~BV(SLEEP);
+    //mode1 |= BV(RESTART);	
+    __s32 res = i2c_smbus_write_byte_data(m_file, MODE1, mode1);
+    if (res < 0) { return res; }
+    return 0;
+
+    __s32 mode2 = i2c_smbus_read_byte_data(m_file, MODE2);
+    if (mode2 < 0) { return mode2; }
+    mode2 |= BV(OUTDRV);
+    res = i2c_smbus_write_byte_data(m_file, MODE2, mode2);
+    if (res < 0) { return res; }
+    return 0;
 }
 
 float pca9685::setUpdateFrequency(float freq_Hz)
 {
-	int prescalValue = (int)((INT_OSC_FREQ / 4096.0f  / freq_Hz)  - 1.0f + 0.5f);
-	if(prescalValue < 3 && prescalValue >  255) return -1.0f;
-	// Read mode1 register 
-	__s32 mode1 = i2c_smbus_read_byte_data(m_file, MODE1);
-	if(mode1 < 0) {
-		printf("Failed to read from MODE1 register.\n");
-		return mode1;
-	}
-	// Check if sleep mode is enabled ).
-	bool sleeping = ((mode1 & BV(SLEEP)) != 0);
-	__s32 res;
-	if(! sleeping) // Enter sleep mode (requirred during prescale update)
-	{
-		res = i2c_smbus_write_byte_data(m_file, MODE1,  (mode1 & ~BV(RESTART) ) | BV(SLEEP) ) ;
-		if(res < 0) {
-			printf("Failed to write to MODE1 register.\n");
-			return res;
-		}
-	}
-	// write new prescale value.
-	res = i2c_smbus_write_byte_data(m_file, PRE_SCALE, (__u8)prescalValue );
-	if(! sleeping) // Restart the module if it was previously put to sleep.
-	{ 
-		res = i2c_smbus_write_byte_data(m_file, MODE1,  (mode1 | BV(RESTART)  ) );
-	}
-	// Return actual frequency
-	return (INT_OSC_FREQ/4096.0f)/(prescalValue +1);
+    int prescalValue = (int)((INT_OSC_FREQ / 4096.0f / freq_Hz) - 1.0f + 0.5f);
+    if (prescalValue < 3 && prescalValue >  255) return -1.0f;
+    // Read mode1 register 
+    __s32 mode1 = i2c_smbus_read_byte_data(m_file, MODE1);
+    if (mode1 < 0) {
+        printf("Failed to read from MODE1 register.\n");
+        return mode1;
+    }
+    // Check if sleep mode is enabled ).
+    bool sleeping = ((mode1 & BV(SLEEP)) != 0);
+    __s32 res;
+    if (!sleeping) // Enter sleep mode (requirred during prescale update)
+    {
+        res = i2c_smbus_write_byte_data(m_file, MODE1, (mode1 & ~BV(RESTART)) | BV(SLEEP));
+        if (res < 0) {
+            printf("Failed to write to MODE1 register.\n");
+            return res;
+        }
+    }
+    // write new prescale value.
+    res = i2c_smbus_write_byte_data(m_file, PRE_SCALE, (__u8)prescalValue);
+    if (!sleeping) // Restart the module if it was previously put to sleep.
+    {
+        res = i2c_smbus_write_byte_data(m_file, MODE1, (mode1 | BV(RESTART)));
+    }
+    // Return actual frequency
+    return (INT_OSC_FREQ / 4096.0f) / (prescalValue + 1);
 }
 
 void pca9685::setPWM(__u8 channel, unsigned short on, unsigned short off)
 {
-	if(on==off)
-	{
-		on = 0;
-		off =0x1000; // Set full off bit
-	}
-	if(off >= 0x0FFF && on == 0)
-	{
-		on = 0x1000; // Set full on bit
-		off =0x0;
-	}
+    if (on == off)
+    {
+        on = 0;
+        off = 0x1000; // Set full off bit
+    }
+    if (off >= 0x0FFF && on == 0)
+    {
+        on = 0x1000; // Set full on bit
+        off = 0x0;
+    }
 
-	m_channels[channel*4 + 0] = on & 0xFF;
-	m_channels[channel*4 + 1] = on >> 8;
-	m_channels[channel*4 + 2] = off & 0xFF;
-	m_channels[channel*4 + 3] = off >> 8;
+    m_channels[channel * 4 + 0] = on & 0xFF;
+    m_channels[channel * 4 + 1] = on >> 8;
+    m_channels[channel * 4 + 2] = off & 0xFF;
+    m_channels[channel * 4 + 3] = off >> 8;
 }
 
 int pca9685::writeAllChannels()
 {
-	__s32 res;
-	__u8 len = I2C_SMBUS_I2C_BLOCK_MAX;
-	// 16 channels of 4 bytes each, we can read 32 bytes in one transaction, so we only need two transactions to transfer all channel data.
-	for(__u8 i = 0; i < 2;++i)
-	{
-		// Read a block of registers.
-		res = i2c_smbus_write_i2c_block_data(m_file,  LED0_ON_L + i*len, len, &m_channels[i*len]);
-		if(res < 0) {
-			printf("Failed to write block\n");
-			return res;
-		}
-	}
-	return 0;
+    __s32 res;
+    __u8 len = I2C_SMBUS_I2C_BLOCK_MAX;
+    // 16 channels of 4 bytes each, we can read 32 bytes in one transaction, so we only need two transactions to transfer all channel data.
+    for (__u8 i = 0; i < 2; ++i)
+    {
+        // Read a block of registers.
+        res = i2c_smbus_write_i2c_block_data(m_file, LED0_ON_L + i*len, len, &m_channels[i*len]);
+        if (res < 0) {
+            printf("Failed to write block\n");
+            return res;
+        }
+    }
+    return 0;
 }
 
 int pca9685::readAllChannels()
 {
-	__s32 res;
-	__u8 len = I2C_SMBUS_I2C_BLOCK_MAX;
-	// 16 channels of 4 bytes each, we can read 32 bytes in one transaction, so we only need two transactions to transfer all channel data.
-	for(__u8 i = 0; i < 2;++i)
-	{
-		// Read a block of registers.
-		res = i2c_smbus_read_i2c_block_data(m_file,  LED0_ON_L + i*len, len, &m_channels[i*len]);
-		if(res < 0) {
-			printf("Failed to read block\n");
-			return res;
-		}
+    __s32 res;
+    __u8 len = I2C_SMBUS_I2C_BLOCK_MAX;
+    // 16 channels of 4 bytes each, we can read 32 bytes in one transaction, so we only need two transactions to transfer all channel data.
+    for (__u8 i = 0; i < 2; ++i)
+    {
+        // Read a block of registers.
+        res = i2c_smbus_read_i2c_block_data(m_file, LED0_ON_L + i*len, len, &m_channels[i*len]);
+        if (res < 0) {
+            printf("Failed to read block\n");
+            return res;
+        }
 
-		//for(__u8 reg = 0; reg < len; ++reg)
-		//{
-		//	printf("Register 0x%2X = 0x%2X (%d)\n", LED0_ON_L + i*len + reg, m_channels[i*len+reg], m_channels[i*len+reg]);
-		//}
-	}
-	return 0;
+        //for(__u8 reg = 0; reg < len; ++reg)
+        //{
+        //	printf("Register 0x%2X = 0x%2X (%d)\n", LED0_ON_L + i*len + reg, m_channels[i*len+reg], m_channels[i*len+reg]);
+        //}
+    }
+    return 0;
 }
 
 void pca9685::printAllRegisters()
 {
-	for (__u8 reg = 0; reg != 255; reg++)
-	{
-		__s32 res = i2c_smbus_read_byte_data(m_file, reg);
-		if (res < 0) {
-			/* ERROR HANDLING: i2c transaction failed */
-		} else {
-			printf("Register 0x%2X = 0x%2X (%d)\n", reg, res, res);
-		}
-	}
+    for (__u8 reg = 0; reg != 255; reg++)
+    {
+        __s32 res = i2c_smbus_read_byte_data(m_file, reg);
+        if (res < 0) {
+            /* ERROR HANDLING: i2c transaction failed */
+        }
+        else {
+            printf("Register 0x%2X = 0x%2X (%d)\n", reg, res, res);
+        }
+    }
 }
